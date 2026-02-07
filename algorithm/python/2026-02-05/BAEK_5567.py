b3de9325bd7950f24575fde53731dcc54d40a7a9
조하원 <godhw1018@naver.com>
2026-02-07 12:31:37 +0900

    2026-02-07_algo_python

--- algorithm/python/2026-02-05/BAEK_5567.py
+++ algorithm/python/2026-02-05/BAEK_5567.py
@@ -1,11 +1,36 @@
-N = int(input())
-M = int(input())
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-friends = [0 * N for _ in range(N)]
+n = int(input().strip())
+m = int(input().strip())
 
-# for _ in range(M):
-#     a, b = map(int, input().split())
-#     friends[a].append(b)
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
+dist = [-1] * (n + 1) 
+q = deque()
 
-print(friends)
+dist[1] = 0
+q.append(1)
+
+while q:
+    now = q.popleft()
+
+    if dist[now] == 2:
+        continue
+
+    for nxt in graph[now]:
+        if dist[nxt] == -1:
+            dist[nxt] = dist[now] + 1
+            q.append(nxt)
+
+result = 0
+for i in range(2, n + 1):
+    if dist[i] == 1 or dist[i] == 2:
+        result += 1
+
+print(result)
