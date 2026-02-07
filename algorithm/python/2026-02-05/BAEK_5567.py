c867096c57d564e63ac9f85d72d0e66cc3257fa3
조하원 <godhw1018@naver.com>
2026-02-07 11:50:17 +0900

    2026-02-07

---
+++ algorithm/python/2026-02-05/BAEK_5567.py
@@ -0,0 +1,11 @@
+N = int(input())
+M = int(input())
+
+friends = [0 * N for _ in range(N)]
+
+# for _ in range(M):
+#     a, b = map(int, input().split())
+#     friends[a].append(b)
+
+
+print(friends)
